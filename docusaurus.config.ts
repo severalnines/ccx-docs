b4ee6b55f10c65df2bf088d25858de6e50edadc6
Wojciech Niemkowski <wojciech@severalnines.com>
2024-09-27 11:59:07 +0200

    gh pages test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.severalnines.com.com',
+  url: 'https://www.github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ccx-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'severalnines', // Usually your GitHub org/user name.
   projectName: 'ccx-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
