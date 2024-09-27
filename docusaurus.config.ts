aef83c60a4403de4e9b6962f2a6cf342528967d7
Wojciech Niemkowski <wojciech@severalnines.com>
2024-09-27 12:09:50 +0200

    warn on broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   projectName: 'ccx-docs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
