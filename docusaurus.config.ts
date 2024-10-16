2f3443334ff9c140a76240b3ea00378e93b1ee60
Alexander Baran <bunin@severalnines.com>
2024-10-16 16:22:28 +0700

    Enable local search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ccx-docs',
 
+  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'severalnines', // Usually your GitHub org/user name.
