a47caad1cb1be4430b17529fb2ec5eb352b76697
Johan Andersson <johan@severalnines.com>
2025-05-07 15:32:02 +0200

    updated

--- sidebars.ts
+++ sidebars.ts
@@ -290,12 +290,12 @@ const sidebars: SidebarsConfig = {
                     'user/Reference/Products/MySQL/Backup',
                     'user/Reference/Products/MySQL/restore',
                     'user/Reference/Products/MySQL/configuration',
-                    'user/Reference/Products/MySQL/Import',
-                    'user/Reference/Products/MySQL/Importing-Data-from-AWS-RDS',
+                    'user/Reference/Products/MySQL/Importing-Data',
                     'user/Reference/Products/MySQL/Importing-Data-from-GCP',
+                    'user/Reference/Products/MySQL/Importing-Data-from-AWS-RDS',
                     'user/Reference/Products/MySQL/limitations',
                     'user/Reference/Products/MySQL/TLS-Connection',
-                    'user/Reference/Products/MySQL/User-Management'
+                    'user/Reference/Products/MySQL/User-Management',
                   ],
                 },
                 {
